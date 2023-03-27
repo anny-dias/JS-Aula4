91f514c95daa2172ef10ce48d2098a113e42d5f3
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-27 11:32:00 -0300

    aula4

---
+++ aula-5/script.js
@@ -0,0 +1,53 @@
+//alert simples
+//alert("Olá Mundo!!")
+
+
+//alert de confirmação
+//let msg = confirm("Voce é um aluno?")
+
+
+//alert passando valores em campos
+//let msg1 = prompt("Digite seu nome")
+//document.write("Meu nome é: ",msg1) 
+//document.write("<br>")
+//document.write("Teste")
+
+
+//condição if
+//if(10 > 7){
+    //document.write("Dez é maior que sete!!!")
+    
+//}
+
+//let valor = 20
+
+//if(valor > 10){
+   //document.write("<br>")
+   //document.write("O " +valor+ " é maior que 10") 
+//}else{
+    //document.write("<br>")
+    //document.write("O valor é menor") 
+//}
+
+/*
+let idade = 20
+if(idade <= 12){
+    document.write("É uma criança")
+}else if (idade <= 17){
+    document.write("É um adolecente")
+}else if(idade <= 29){
+    document.write("É um jovem")
+}else{
+    document.write("É um adulto")
+}
+*/
+
+
+let valor = prompt("Digite um valor")
+if(valor > 10){
+    document.write("O valor é maior")
+}else if(valor == 10){
+    document.write("O valor é igual")
+}else{
+    document.write("O valor é menor")
+}
